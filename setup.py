6f564d7fae3894d78d459f1d006aa4e5c73bddc1
Simon J. Lequar <slequar@caltech.edu>
2020-09-15 10:03:00 -0500

    testing third time

--- setup.py
+++ setup.py
@@ -4,10 +4,13 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pyrp-slequar", # Replace with your own username
-    version="0.0.2",
+    name="rankedpairs", # Replace with your own username
+    version="0.0.3",
     author="Simon Lequar",
     author_email="simon.lequar@gmail.com",
+	license='MIT',
+	keywords='voting',
+
     description="A lightweight ranked pairs python package",
     long_description=long_description,
     long_description_content_type="text/markdown",
