00f1ebdf22e3f4e9f5497f005d440330f2cef232
Simon J. Lequar <slequar@caltech.edu>
2020-09-15 20:17:23 -0500

    1.0.0 details mostly done

--- pyrp/election.py
+++ pyrp/election.py
@@ -2,6 +2,7 @@ from . import utils as _utils
 from . import rp as _rp
 import json as _json
 from functools import reduce as _reduce
+import ast as _ast
 
 class Election:
 	"""
@@ -13,16 +14,27 @@ class Election:
 	lead to missing candidates if no one ranked that candidate). 
 	"""
 
-	def __init__(self, votes=[]):
+	def __init__(self, votes=[], method=_rp):
+		"""
+		constructor, optionally takes a vote list of dicts, will eventually
+		be able to accept multiple voting methods once implemented
+		"""
+		self.method = method
 		self.votes = votes
 		self._update_on_vote()
 
 	def _update_on_vote(self):
+		"""
+		Helper to reset state when votes change
+		"""
 		self.single_winner = None
 		self.full_order = None
 		self.candidates, self.votes = _utils.gen_cand_and_clean(self.votes)
 
 	def set_votes(self, new_votes):
+		"""
+		Public method to change the votes in an Election
+		"""
 		self.votes = new_votes
 		self._update_on_vote()
 	
@@ -33,24 +45,31 @@ class Election:
 		return self.votes
 
 	def get_winner(self):
+		"""
+		Computes and stores the single winner of an election using the
+		method specified (ranked pairs is the only method at present)
+		"""
 		if self.single_winner is not None:
 			return self.single_winner
-		temp = _rp.run(self.candidates, self.votes)
+		temp = self.method.run(self.candidates, self.votes)
 		self.single_winner = temp
 		return self.single_winner
 
 	def get_full_order(self):
+		"""
+		Computes and stores the full results of an election using the 
+		specified method of the Election
+		"""	
 		if self.full_order is not None:
 			return self.full_order
-		temp = _rp.full_order(self.candidates, self.votes)
+		temp = self.method.full_order(self.candidates, self.votes)
 		self.full_order = temp
 		return self.full_order
 
 	def load(self, votes):
 		"""
 		This method loads a JSON string representing a list of dicts into
-		the votes of the Election object. It can also accept an already loaded
-		list of dicts. 
+		the votes of the Election object. It can also accept a list of dicts
 		"""
 		# Case of JSON string, otherwise assume list
 		if type(votes) == str:
@@ -72,6 +91,24 @@ class Election:
 		self._update_on_vote()
 
 
+	def load_from_file(self, filename):
+		"""
+		This method can load a file into the Election object, the file must
+		contain one dictionary per line, formatted as a literal dictionary
+		or as a JSON object
+		"""
+		with open(filename, 'r') as f:
+			try:
+				votes = list(map(_ast.literal_eval, f.readlines()))
+				assert(type(votes) == list and type(votes[0]) == dict)
+				self.votes = votes
+				self._update_on_vote()
+			except (SyntaxError, ValueError):
+				raise ValueError("Votes not formatted correctly, cannot \
+								  be evaluated to list of dict")
+			except AssertionError:
+				raise ValueError("Votes not formatted correctly, must be \
+								  one dictionary per line, not separated")
 
 
 
