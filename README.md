ad98d8cf7c87fec026e670e78e9ca47249dfaa1e
Simon J. Lequar <slequar@caltech.edu>
2020-09-15 20:25:15 -0500

    Last cleanup before 1.0.0

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-# PyRP
+# PyRP (rankedpairs)
 A Ranked Pairs Python Module
 
 This module will eventually contain implementations of all popular ranked choice voting methods in a single lightweight python package. At present, only Ranked Pairs has been implemented.
+
+```console
+pip install rankedpairs
+```
+
+
--- rankedpairs/__init__.py
+++ rankedpairs/__init__.py
@@ -7,7 +7,11 @@ There are two main components to this, the Ranked Pairs module (rp) and the
 Election class (Election). rp contains all the logic for Ranked Pairs, and 
 Election is a wrapper class to make working with the data a bit easier. 
 
-There is more documentation in each module (ie help(rankedpairs.Election))
+A common way to import this module would be:
+
+import rankedpairs as pyrp
+
+There is more documentation in each module (ie help(rp.Election))
 
 Written by Simon Lequar, Dabney '22
 """
